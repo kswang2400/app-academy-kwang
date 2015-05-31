34a8249ab9aca1d6926d0dfc5c4118f2e0612181
Kevin <kevinwang2400@gmail.com>
2015-05-31 09:13:33 -0700

    clean up files

--- week-3-day-3/db/migrate/20150507001233_add_premium_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPremiumToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :premium, :boolean, default: false
-  end
-end
--- week-3-day-3/lib/tasks/clean.rake
+++
@@ -1,7 +0,0 @@
-namespace :clean do
-  desc "TODO"
-  task prune: :environment do
-    ShortenedUrl.prune
-  end
-
-end
